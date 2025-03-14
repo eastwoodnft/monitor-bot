e2b4cfbd934d5ac6d666b88d9727ff1fbcdbfdea
eastwood <160615416+eastwoodnft@users.noreply.github.com>
2025-03-14 16:33:33 -0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,10 +8,13 @@ load_dotenv()
 # Configuration
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
 VALIDATOR_CONSENSUS_ADDRESS = os.getenv("VALIDATOR_CONSENSUS_ADDRESS")
 VALIDATOR_OPERATOR_ADDRESS = os.getenv("VALIDATOR_OPERATOR_ADDRESS")
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
-UNION_RPC = "http://161.35.98.109:26657"
-UNION_REST_API = "http://161.35.98.109:1317"
+VALIDATOR_RPC = os.getenv("VALIDATOR_RPC")
+VALIDATOR_API = os.getenv("VALIDATOR_API")
+UNION_RPC = "https://rpc.testnet-9.union.build/"
+UNION_REST_API = "https://rest.testnet-9.union.build"
+UNION_GRPC = "https://grpc.testnet-9.union.build"
 SLASHING_WINDOW = 100
 SLASHING_THRESHOLD = 0.20  # 20% threshold for slashing alert
